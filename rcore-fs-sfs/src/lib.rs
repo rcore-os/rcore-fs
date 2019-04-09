1cc5fd90bba3615bf108b01ae9a25afa333e6a82
Tianyu Gao <gaotianyu1350@126.com>
2019-04-09 08:00:02 +0000

    remove creating dev in root_inode

--- rcore-fs-sfs/src/lib.rs
+++ rcore-fs-sfs/src/lib.rs
@@ -868,10 +868,10 @@ impl vfs::FileSystem for SimpleFileSystem {
     }
 
     fn root_inode(&self) -> Arc<vfs::INode> {
-        // self.get_inode(BLKN_ROOT)
-        let root = self.get_inode(BLKN_ROOT);
-        root.create("dev", vfs::FileType::Dir, 0).expect("fail to create dev"); // what's mode?
-        return root;
+        self.get_inode(BLKN_ROOT)
+        // let root = self.get_inode(BLKN_ROOT);
+        // root.create("dev", vfs::FileType::Dir, 0).expect("fail to create dev"); // what's mode?
+        // return root;
     }
 
     fn info(&self) -> vfs::FsInfo {
